c208c7cb31cec4d30387e97e58380f20897d548d
Jasper Wong <yom092090@gmail.com>
2024-05-04 16:18:23 +0800

    enabled i2c2; write Boosting or Max V_Out to OLED

--- CubeIDE/3300 project/Core/Src/main.c
+++ CubeIDE/3300 project/Core/Src/main.c
@@ -49,6 +49,7 @@ DMA_HandleTypeDef hdma_adc1;
 CAN_HandleTypeDef hcan;
 
 I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
 
 RTC_HandleTypeDef hrtc;
 
@@ -86,6 +87,7 @@ static void MX_TIM5_Init(void);
 static void MX_CAN_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_TIM4_Init(void);
+static void MX_I2C2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -136,6 +138,7 @@ int main(void)
   MX_CAN_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
@@ -152,9 +155,9 @@ int main(void)
   ssd1306_Init();
   ssd1306_Fill(White);
   ssd1306_SetCursor(2,0);
-  ssd1306_WriteString("ELEC3300 Solar", Font_11x18, Black);
+  ssd1306_WriteString("ELEC3300 Solar", Font_6x8, Black);
   ssd1306_SetCursor(2, 18*2);
-  ssd1306_WriteString("Group 12", Font_11x18, Black);
+  ssd1306_WriteString("Group 12", Font_6x8, Black);
   ssd1306_UpdateScreen();
   HAL_Delay(1000);
   /* USER CODE END 2 */
@@ -166,7 +169,7 @@ int main(void)
     HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adc_data, 14);
     // HAL_Delay(2);
     // ssd1306_SetCursor(2, 18);
-    // ssd1306_WriteString(message, Font_11x18, Black);
+    // ssd1306_WriteString(message, Font_6x8, Black);
     // ssd1306_UpdateScreen();
     tickstart = HAL_GetTick();
     while ((HAL_GetTick() - tickstart) < 2)
@@ -456,6 +459,40 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
 
 }
 
